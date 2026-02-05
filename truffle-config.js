1d605426d48acb0037047434f618a33933c51d0a
Yug Buha <yugbuha001@gmail.com>
2026-02-05 21:51:23 +0530

    deployed contract on sepolia testnet using alchemy

--- truffle-config.js
+++ truffle-config.js
@@ -1,9 +1,26 @@
+require('dotenv').config();
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 7545,
-      network_id: "*" // Match any network id
+      network_id: "*"
+    },
+    sepolia: {
+      provider: () => new HDWalletProvider(
+        process.env.MNEMONIC,
+        `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`
+      ),
+      network_id: 11155111,
+      gas: 4500000,  // Lower this
+      gasPrice: 10000000000,  // Set fixed gas price (10 gwei)
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true,
+      networkCheckTimeout: 100000,  // Add these
+      deploymentPollingInterval: 10000  // Add these
     }
   },
   compilers: {
